fc8efd30f8fc330391a7862d58f737b37de57259
ofiralon <ofir.alon@theculturetrip.com>
2021-04-25 14:26:46 +0300

    more comments

--- src/main/kotlin/a_basics/6.dispatchers.kt
+++ src/main/kotlin/a_basics/6.dispatchers.kt
@@ -4,9 +4,14 @@ import kotlinx.coroutines.*
 
 //dispatchers - determines which thread or thread pool the coroutine runs on
 //if you want to be more accurate
-//we have: Main(ui thread). main dispatcher needs to be defined in Gradle (we get this automatically in android studio)
-//default - usually for cpu intensive work
+//Main(ui thread). main dispatcher needs to be defined in Gradle (we get this automatically in android studio)
+//Main vs Main.immediate - handler(mainLooper).post vs Activity.runOnUiThread
+//default - usually for cpu intensive work - thread pool with maxThreads = max(2, NUM_OF_CPU_CORES)
+//
 //IO - useful for network communication or reading files etc
+//by default - Thread pool with maxThreads = max(64, NUM_OF_CPU_CORES)
+//we can adjust it - doc
+//
 //unconfined - starts the coroutine in the inherited dispatcher that called it - if we don't specified a dispatcher
 //we get the current one (can't be controlled!!)
 //newSingleThreadContext("MyThread") - forces creation of a new thread - use only for a specific scenarios
