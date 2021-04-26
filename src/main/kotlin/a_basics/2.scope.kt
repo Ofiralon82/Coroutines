a9250e053f954d99a3f8ae53dbd252d81b2f09b8
ofiralon <ofir.alon@theculturetrip.com>
2021-04-27 01:27:22 +0300

    more comments added

--- src/main/kotlin/a_basics/2.scope.kt
+++ src/main/kotlin/a_basics/2.scope.kt
@@ -10,6 +10,13 @@ import kotlinx.coroutines.*
 
 //https://stackoverflow.com/questions/53535977/coroutines-runblocking-vs-coroutinescope - nice discussion
 //summary:
+//more:
+//CoroutineScope and CoroutineContext are almost the same (scope wraps the context). but different purposes -
+//CoroutineScope uses to manage coroutines, to lunch new coroutines and CoroutineContext uses to control the execution environment
+//(CoroutineScope is the builder while coroutineScope is something like launch)
+//everytime we alter the coroutineContext we basically create new coroutine scope
+//
+//summary and examples right after:
 //when we use withContext() blocks, we remain within the same scope of the same coroutine, we don't get concurrency (just switching context)
 //and structured concurrency works
 
@@ -17,6 +24,45 @@ import kotlinx.coroutines.*
 
 //when we launch new coroutine within parent coroutine but we don't use same scope but external scope for example - we still get concurrency BUT
 //not structured concurrency
+
+//again with examples:
+//runBlocking {
+//    val scopeJob = Job()
+//    val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.Default)
+//    val job = scope.launch(CoroutineName("coroutine")) {
+//        delay(500)
+//        println("before nested")
+//        withContext(Dispatchers.IO) {
+//            delay(500)
+//            printJobsHierarchy(scopeJob)
+//            println("nested")
+//        }
+//        println("after nested")
+//    }
+//above we have job Hierarchy:
+// - scope Job
+//   - coroutine Job
+//     - context Job
+// - no concurrency there BUT we do change thread from within withContext. and the job will wait for it in order to finish (structured concurrency)
+//
+//runBlocking {
+//    val scopeJob = Job()
+//    val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.Default)
+//    val job = scope.launch(CoroutineName("coroutine")) {
+//        delay(500)
+//        println("before nested")
+//        val nestedJob = launch(Dispatchers.IO) {
+//            delay(500)
+//            printJobsHierarchy(scopeJob)
+//            println("nested")
+//        }
+//        println("after nested")
+//    }
+//above - the job hierarchy stays the same BUT the launch block is now concurrency running. we still have structured concurrency - means that
+//the launch (create new coroutine) uses the same scope of the parent coroutine.
+
+//NonCancellable is "detaching" withContext (and ONLY withContext) from its parent job - we can get the same effect by simply create new Job()
+
 
 fun main() {
     println("program execution will now block")
