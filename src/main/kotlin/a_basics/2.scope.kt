ddc73a56d7c1981a488bf4a4baa53b03d9be9472
ofiralon <ofir.alon@theculturetrip.com>
2021-04-27 00:57:54 +0300

    more comments added

--- src/main/kotlin/a_basics/2.scope.kt
+++ src/main/kotlin/a_basics/2.scope.kt
@@ -9,6 +9,15 @@ import kotlinx.coroutines.*
 //here - the runBlocking block the execution and the last print wait until all finished.
 
 //https://stackoverflow.com/questions/53535977/coroutines-runblocking-vs-coroutinescope - nice discussion
+//summary:
+//when we use withContext() blocks, we remain within the same scope of the same coroutine, we don't get concurrency (just switching context)
+//and structured concurrency works
+
+//when we launch new coroutine within parent coroutine (simply launch() {}) - we get concurrency and structured concurrency
+
+//when we launch new coroutine within parent coroutine but we don't use same scope but external scope for example - we still get concurrency BUT
+//not structured concurrency
+
 fun main() {
     println("program execution will now block")
     runBlocking {
